8af336bdaaae5d1277558900d2b7a3bfc0b52698
TheRant-man <55584170+TheRant-man@users.noreply.github.com>
2024-05-25 23:14:46 +0300

    Revert "music stuff"
    This reverts commit bf81fa8261c59663908606d07a771afcd7937a1b.

--- hooks/musicmanager.lua
+++
@@ -1,6 +0,0 @@
-local jukebox_default_tracks_ori_bluewave = MusicManager.jukebox_default_tracks
-function MusicManager:jukebox_default_tracks()
-    local default_options = jukebox_default_tracks_ori_bluewave(self)
-    default_options.heist_narr_bluewave_name = "music_bluewave"
-    return default_options
-end
